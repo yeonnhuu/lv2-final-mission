9659fac52ca6d3993d064be98716dc0a26e84422
yeonnhuu <yeonnhuu@gmail.com>
2025-06-16 14:15:57 +0900

    :recycle: refactor: 생성자 롬복 추가

--- src/main/java/finalmission/common/argumentresolver/LoginMemberArgumentResolver.java
+++ src/main/java/finalmission/common/argumentresolver/LoginMemberArgumentResolver.java
@@ -6,19 +6,17 @@ import finalmission.service.AuthService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@RequiredArgsConstructor
 public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
 
     private final AuthService authService;
-
-    public LoginMemberArgumentResolver(AuthService authService) {
-        this.authService = authService;
-    }
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
